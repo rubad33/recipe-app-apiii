8a78ff6bf40ce3a3768e69d443b934f31b0b22b7
Syed Rubad <syedmaskat420@gmail.com>
2025-11-12 02:14:31 +0600

    Update Docker setup and rebuild containers

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+sample tests 
+
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Tests for the calculator module."""
+
+    def test_add_numbers(self):
+        """Test adding two numbers."""
+        res = calc.add(3, 5)
+        self.assertEqual(res, 8)
+
+    def test_add_negative_numbers(self):
+        """Test adding two negative numbers."""
+        res = calc.add(-2, -3)
+        self.assertEqual(res, -5)    
+
+    
